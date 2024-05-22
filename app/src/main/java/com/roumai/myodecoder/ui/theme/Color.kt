0aef708e354fd3665f68c6a8b062c990e57e8b60
snowcharm <snowcharmqq@gmail.com>
2024-05-22 18:09:01 +0800

    feat: add colors

--- app/src/main/java/com/roumai/myodecoder/ui/theme/Color.kt
+++ app/src/main/java/com/roumai/myodecoder/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val ColorWhite = Color.White
+val ColorGraphite = Color(0xFF313234)
+val ColorLightGray = Color.LightGray
+val ColorDarkGray = Color.DarkGray.copy(alpha = 0.4f)
